eca8b856ba5d8945b03ea45c4e1113f7c86c04d5
muhammadaffan <muhammadaffanpvt@gmail.com>
2026-02-01 15:51:15 +0530

    add grid mesh and a status badge

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { cn } from "../lib/utils.js";
 import TerminalComp from "../components/TerminalComp";
 import { MoveRight } from "lucide-react";
 import { Link } from "react-router";
 function Home() {
     return (
-        <section className="min-h-screen bg-[#18181b] flex  justify-center px-6 ">
+        <section className={cn(
+            "min-h-screen bg-[#18181b] flex  justify-center px-6 ",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,rgba(255,255,255,0.04)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.04)_1px,transparent_1px)]"
+
+        )}>
             <div className="max-w-3xl w-full text-center mt-36 md:mt-28">
+                <div className="inline-flex mb-3 items-center gap-2 px-3 py-1.5 rounded-full bg-white/5 border border-white/10">
+                    {/* Pulsing dot */}
+                    <span className="relative flex h-2 w-2">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-orange-400 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-2 w-2 bg-orange-500"></span>
+                    </span>
+
+                    {/* Text */}
+                    <span className="text-sm text-white/80">
+                        Powered by <span className="font-semibold text-white">Groq AI</span>
+                    </span>
+                </div>
                 <h1 className="text-4xl  font-bold  md:text-5xl leading-snug  md:leading-[1.1] text-white tracking-tight">
                     Let your git diff write better pull requests
                     <br className="md:hidden" /> with -{' '}
